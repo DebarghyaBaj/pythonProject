7a802c766d8b40d4f9b604f85306202a5e43a1da
Debarghya Baj <bajdebarghya@gmail.com>
2022-06-20 12:35:13 +0530

    09/06/22

--- deb/listEx/list2.py
+++ deb/listEx/list2.py
@@ -1,5 +1,25 @@
 thisList = list(("Apple", "Banana", "Cherry"))
+myList = [1, 2, 3]
 print(thisList)
 
 thisList[1:3] = ["Debarghya", "Baj"]
 print(thisList)
+
+thisList.insert(1, "Squash")
+print(thisList)
+
+for x in range(len(thisList)):
+    print(thisList[x])
+
+thisList.extend(myList)
+print(thisList)
+
+for x in thisList:
+    print(x)
+
+thisList.remove("Apple")
+print(thisList)
+
+thisList.pop(1)
+print(thisList)
+
